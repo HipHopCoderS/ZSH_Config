ed788a9a03f6413b3a3bd2bb43a28008f9901e1d
xinlei <xinlei@test.com>
2018-10-26 11:18:05 +0800

    modify script

--- setup.sh
+++ setup.sh
@@ -1,32 +1,56 @@
-#!/bin/sh
+#!/bin/zsh
 
 omz=~/.oh-my-zsh
 
+printResult(){
+    if [[ $1 -eq 0 ]]; then
+        echo "$2-------Success"
+    else
+        echo "$2-------Fail"
+    fi
+}
+
+echo "oh-my-sh install begin ####"
 # install for oh-my-zsh
-if [[ ! -d  $omz ]];then
+if [[ ! -e  $omz/oh-my-zsh.sh ]];then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    printResult 0 oh-my-zsh
 else
-    echo "oh-my-zsh install success!"
+    printResult 0 oh-my-zsh
 fi
 
 
 # install theme for oh-my-zsh
 if [[  ! -d  $omz/custom/themes/powerlevel9k ]];then
     git clone https://github.com/bhilburn/powerlevel9k.git $omz/custom/themes/powerlevel9k
+    printResult 0 oh-my-zsh-theme
 else
-    echo "themes install success!"
+    printResult 0 oh-my-zsh-theme
 fi
 
 
 # install plugin
-if [[  ! -d  $omz/custom/themes/powerlevel9k ]];then
-    git clone https://github.com/zsh-users/zsh-autosuggestions $omz/plugins/zsh-autosuggestions
+if [[  ! -d  $omz/custom/plugins/zsh-autosuggestions ]];then
+    git clone https://github.com/zsh-users/zsh-autosuggestions $omz/custom/plugins/zsh-autosuggestions
+    printResult 0 plugin-zutosuggestions 
+else
+    printResult 0 plugin-zutosuggestions 
 fi
 
-cp ./.zshrc   ~/.zshrc
-cp ./.tmux.conf   ~/.tmux.conf
-cp ./.todo.cfg   ~/.todo.cfh
-source ~/.zshrc
 
+if [[ ! -d ./autojump ]];then
+    git clone git://github.com/wting/autojump.git
+else
+    cd ./autojump
+    ./install.py
+    printResult 0 autojump 
+    cd ../
+fi
+
+cp -r -f ./.zshrc   ~/.zshrc
+cp -r -f ./.tmux.conf   ~/.tmux.conf
+cp -r -f ./.todo.cfg   ~/.todo.cfh
+source ~/.zshrc
+echo "oh-my-sh install end ####"
 
 
